f8b76679fff4d5aceaeb68bf9c7973097a83e821
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-05-03 09:21:08 +0000

    Parameterized tests with ValueSource annotations

--- my-app/src/test/java/com/linkedin/app/StringUtilsTest.java
+++ my-app/src/test/java/com/linkedin/app/StringUtilsTest.java
@@ -3,32 +3,20 @@ package com.linkedin.app;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringUtilsTest {
 
-  @Test
-  void isPalindromeWithEmptyString() {
-    assertTrue(StringUtils.isPalindrome(""));
+  @ParameterizedTest
+  @ValueSource(strings = { "", "a", "racecar", "RaceCar" })
+  public void isPalindrome(String str) {
+    assertTrue(StringUtils.isPalindrome(str));
   }
 
-  @Test
-  void isPalindromeWithSingleCharacter() {
-    assertTrue(StringUtils.isPalindrome("a"));
-  }
-
-  @Test
-  void isPalindromeWithPalindromeString() {
-    assertTrue(StringUtils.isPalindrome("racecar"));
-  }
-
-  @Test
-  void isPalindromeWithNonPalindromeString() {
-    assertFalse(StringUtils.isPalindrome("hello"));
-  }
-
-  @Test
-  void isPalindromeWithMixedCasePalindromeString() {
-    assertTrue(StringUtils.isPalindrome("RaceCar"));
+  @ParameterizedTest
+  @ValueSource(strings = { "hello", "world", "java", "JUnit" })
+  public void isNotPalindrome(String str) {
+    assertFalse(StringUtils.isPalindrome(str));
   }
 }

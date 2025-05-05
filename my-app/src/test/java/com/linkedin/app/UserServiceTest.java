b29d805cdd89342f73b7f4d09f6589ad0c173168
Paulo Henrique Pontes <115827846+phpontes@users.noreply.github.com>
2025-05-05 16:25:25 +0000

    mocking an object in Java

--- my-app/src/test/java/com/linkedin/app/UserServiceTest.java
+++ my-app/src/test/java/com/linkedin/app/UserServiceTest.java
@@ -1,22 +1,29 @@
 package com.linkedin.app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
+
+  @Mock
+  private UserRepository userRepositoryMock;
+
+  @InjectMocks
+  private UserService underTest;
 
   @Test
   public void findUserById() {
-    UserRepository userRepositoryMock = mock(UserRepository.class);
 
     User mockUser = new User("1", "John Doe");
     when(userRepositoryMock.findById("1")).thenReturn(mockUser);
-
-    UserService underTest = new UserService(userRepositoryMock);
 
     User result = underTest.findUserById("1");
 
